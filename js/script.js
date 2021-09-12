e666e3d70310eeeea6c75929403b62cf5a7cb840
Galaxy Littlepaws <34807062+GalaxyLittlepaws@users.noreply.github.com>
2021-09-11 23:14:15 -0400

    Fix depreciated ".load()" issue
    Fix depreciated ".load()" code breaking the theme

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
 	//animation effect(waypoint)
 //paste this code under head tag or in a seperate js file.
  // Wait for window load
-$(window).load(function() {
+$(window).on('load', function() {
   // Animate loader off screen
   $(".se-pre-con").fadeOut("slow");;
   
