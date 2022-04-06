0edaef4500c3ea9c589451b1b294acb4a1c9d713
Hung Tran <nguyentieuhau@gmail.com>
2022-04-06 18:13:39 +0700

    Updated the scripts to latest verions

--- js/script.js
+++ js/script.js
@@ -12,38 +12,12 @@ $(window).scroll(function() {
 
 $(document).ready(function(){
   $('.bxslider').bxSlider();
-});
 
-	//animation effect(waypoint)
-//paste this code under head tag or in a seperate js file.
- // Wait for window load
-$(window).load(function() {
   // Animate loader off screen
   $(".se-pre-con").fadeOut("slow");;
   
 
-            function onScrollInit( items, trigger ) {
-                items.each( function() {
-                var osElement = $(this),
-                    osAnimationClass = osElement.attr('data-os-animation'),
-                    osAnimationDelay = osElement.attr('data-os-animation-delay');
-                  
-                    osElement.css({
-                        '-webkit-animation-delay':  osAnimationDelay,
-                        '-moz-animation-delay':     osAnimationDelay,
-                        'animation-delay':          osAnimationDelay
-                    });
-
-                    var osTrigger = ( trigger ) ? trigger : osElement;
-                    
-                    osTrigger.waypoint(function() {
-                        osElement.addClass('animated').addClass(osAnimationClass);
-                        },{
-                            triggerOnce: true,
-                            offset: '90%'
-                    });
-                });
-            }
+            
 
             onScrollInit( $('.os-animation') );
             onScrollInit( $('.staggered-animation'), $('.staggered-animation-container') 
@@ -52,3 +26,26 @@ $(window).load(function() {
   
 );
 });
+
+function onScrollInit( items, trigger ) {
+  items.each( function() {
+  var osElement = $(this),
+      osAnimationClass = osElement.attr('data-os-animation'),
+      osAnimationDelay = osElement.attr('data-os-animation-delay');
+    
+      osElement.css({
+          '-webkit-animation-delay':  osAnimationDelay,
+          '-moz-animation-delay':     osAnimationDelay,
+          'animation-delay':          osAnimationDelay
+      });
+
+      var osTrigger = ( trigger ) ? trigger : osElement;
+      
+      osTrigger.waypoint(function() {
+          osElement.addClass('animated').addClass(osAnimationClass);
+          },{
+              triggerOnce: true,
+              offset: '90%'
+      });
+  });
+}
